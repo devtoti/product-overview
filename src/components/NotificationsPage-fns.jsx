1c33775cb5c1f78a3faaf3d7f4b84974f7a84780
devtoti <96400225+devtoti@users.noreply.github.com>
2022-11-21 19:01:24 -0600

    Include useReducer and useContext for better state management

--- src/components/NotificationsPage-fns.jsx
+++ src/components/NotificationsPage-fns.jsx
@@ -1,69 +1,75 @@
-import React, { useEffect, useState, useRef} from 'react'
+import React, { useEffect, useState, useRef, useContext } from 'react'
+import { useDispatch } from './NotificationsPage'
 
-export default function NewEntry({entries, handleNotifications}) {
-    const [entry, setEntry] = useState(users.generateEntry())
+export default function NewEntry({ entry, children }) {
     const [isRead, setIsRead] = useState(false)
     const ref = useRef()
-    useEffect(() => {
-        const notifCount = document.querySelectorAll(".not-read").length
-        console.log(notifCount)
-        // handleNotifications(notifCount)
-        // handleNotifications(2)
-        console.log(typeof handleNotifications)
-    },[entries, isRead])
-
+    const dispatch = useDispatch()
     function markAsRead() {
-        setIsRead(a => !a)
-       }
-   
-    useEffect(() => {
-        console.log(entry)
-    }, [])
+        dispatch({
+            type: 'read',
+            id: entry.id,
+            entry: entry
+        })
+       
+    }
+
+
     return (
-        <div ref={ref} className={isRead ? 'user-notification' : 'user-notification not-read'} onClick={markAsRead}>
+        <div ref={ref} className={entry.read ? 'user-notification' : 'user-notification not-read'} onClick={markAsRead}>
+
             <img src={entry.user.avatar} alt="" />
             <div>
                 <h3>{entry.user.name}</h3>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 close-icon"
+                onClick={() => {
+                    dispatch({
+                        type: 'delete',
+                        id: entry.id
+                    })
+                }}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
                 <h4>{entry.action}</h4>
                 <h5></h5>
-                {/* <p><cite>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iure exercitationem dolore beatae soluta nisi blanditiis fugiat amet veniam odio?</cite></p> */}
             </div>
         </div>
     )
 }
+{/* <p><cite>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iure exercitationem dolore beatae soluta nisi blanditiis fugiat amet veniam odio?</cite></p> */ }
 
 
 
 export const users = {
     names: [
         {
-            name:'Kimberly Smith',
-            avatar:"notifications/assets/images/avatar-kimberly-smith.webp"
+            name: 'Kimberly Smith',
+            avatar: "notifications/assets/images/avatar-kimberly-smith.webp"
         },
         {
             name: 'Nathan Peterson',
-            avatar:"notifications/assets/images/avatar-nathan-peterson.webp",
+            avatar: "notifications/assets/images/avatar-nathan-peterson.webp",
 
         },
         {
-            name:'Jacob Thompson',
-            avatar:"notifications/assets/images/avatar-jacob-thompson.webp"
+            name: 'Jacob Thompson',
+            avatar: "notifications/assets/images/avatar-jacob-thompson.webp"
         },
         {
-            name:'Angela Gray',
-            avatar:"notifications/assets/images/avatar-angela-gray.webp"
+            name: 'Angela Gray',
+            avatar: "notifications/assets/images/avatar-angela-gray.webp"
         },
         {
-            name:'Mark Webber',
-            avatar:"notifications/assets/images/avatar-mark-webber.webp"
+            name: 'Mark Webber',
+            avatar: "notifications/assets/images/avatar-mark-webber.webp"
         },
         {
-            name:'Mark Webber',
-            avatar:"notifications/assets/images/avatar-mark-webber.webp"
+            name: 'Mark Webber',
+            avatar: "notifications/assets/images/avatar-mark-webber.webp"
         },
         {
-            name:'El Patrón',
-            avatar:"notifications/assets/images/avatar-rizky-hasanuddin.webp"
+            name: 'El Patrón',
+            avatar: "notifications/assets/images/avatar-rizky-hasanuddin.webp"
         }
     ],
     actions: [
@@ -74,21 +80,22 @@ export const users = {
         'commented on your picture',
         'left the group'
     ],
- 
-    generateEntry: function() {
-        function random(key) { 
+
+    generateEntry: function () {
+        function random(key) {
             const ix = Math.floor(Math.random() * key.length)
             return key[ix]
         };
 
-            const user = random(this.names)
-            const action = random(this.actions)
-            const avatar = random(this.names)
-         
+        const user = random(this.names)
+        const action = random(this.actions)
+        const avatar = random(this.names)
+
         return ({
-          user,
-          action,
-          avatar
+            user,
+            action,
+            read: false
+
         })
     }
 
--- src/components/NotificationsPage.jsx
+++ src/components/NotificationsPage.jsx
@@ -1,60 +1,133 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, useReducer, useContext } from 'react'
 import NewEntry, { users } from './NotificationsPage-fns'
 import '../notifications.css'
+import { createContext } from 'react'
+const TasksDispatchContext = createContext(null)
 
 function NotificationsPage() {
-    const [entries, setEntries] = useState(0)
+    const [entryId, setEntryID] = useState(1)
     const [notifications, setNotifications] = useState(0)
     const [allRead, setAllRead] = useState(false)
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'add':
+                setAllRead(false)
+                setEntryID(prev => ++prev)
+                const newParam = users.generateEntry()
+                return [...state, { ...newParam, id: entryId }];
 
+            case 'delete':
+                if (action.deleteAll) {
+                    setAllRead(false)   
+                    return []
+                }
+                return state.filter(st => st.id !== action.id);
+
+            case 'read':
+                setAllRead(false)
+                const newState = state.map((post, ix) => {
+                    if (post.id === action.id) {
+                        if (post.read) return ({ ...post, read: false })
+                        else return ({ ...post, read: true })
+                    }
+                    return post
+                })
+                return newState;
+            case 'readAll':
+                if (action.readAll)
+                    return state.map(post => {
+                        post.read = true
+                        return post
+                    })
+                return state.map(post => {
+                    post.read = false
+                    return post
+                })
+
+            default:
+                return state;
+        }
+    }
+
+    const [tasks, dispatch] = useReducer(reducer, [users.generateEntry()])
+
+    useEffect(() => {
+        if (allRead) {
+            setNotifications(0)
+            return
+        }
+        let count = tasks.reduce((total, notification) => {
+            if (!notification.read) return total + 1
+            return total
+        }, 0)
+        setNotifications(count)
+    }, [tasks, allRead])
 
     const addNewEntry = () => {
-        setEntries(prev => ++prev)
+        dispatch({
+            type: 'add',
+            extra: 'extra-param'
+        })
     }
-    function handleNotifications(num) {
-        setNotifications(num)
-    }
+
     function markAllRead() {
         setAllRead(prev => !prev)
         if (!allRead) {
-            document.querySelectorAll(".user-notification").forEach(elem => {
-                elem.classList.remove('not-read')
+            return dispatch({
+                type: 'readAll',
+                readAll: true
             })
         }
         else {
-            document.querySelectorAll(".user-notification").forEach(elem => {
-                elem.classList.add('not-read')
-
+            return dispatch({
+                type: 'readAll',
+                readAll: false
             })
         }
     }
 
+
     return (
-        <div className='notifications'>
-            <section className='notifications_header'>
-                <h1>Notifications</h1>
-                <article>{notifications}</article>
-                <h6 onClick={markAllRead}>
-                    {allRead ? "Mark all as unread" : "Mark all as read"}
-                </h6>
-            </section>
-            <section className='notifications_body'>
+        <TasksDispatchContext.Provider value={dispatch}>
+            <div className='notifications'>
+                <section className='notifications_header'>
+                    <h1>Notifications</h1>
+                    <article>{notifications}</article>
+                        {allRead ? 
+                        <>
+                        <h5 onClick={() => dispatch({type: 'delete', deleteAll: true})}>Clear</h5>
+                         <h6 onClick={markAllRead}>
+                         Mark all as unread 
+                         </h6>
+                        </>
+                         : 
+                         <h6 onClick={markAllRead}>
+                         Mark all as read 
+                         </h6>
+                         }
+                   
+                </section>
+                <section className='notifications_body'>
 
-                <NewEntry />
-                {Array(entries).fill(0).map((entry, ix) => {
-                    return <NewEntry key={ix} entries={entries}
-                    // handleNotifications={num => handleNotifications(num)}
-                    />
-                })}
-            </section>
-            <section className='notifications_footer'>
+                    {tasks.map((post, id) => {
+                        return <NewEntry
+                            key={id}
+                            entry={post} />
+                    })}
+                </section>
+                <section className='notifications_footer'>
 
-                <button onClick={addNewEntry}>+ New Entry</button>
+                    <button onClick={addNewEntry}>+ New Entry</button>
 
-            </section>
-        </div>
+                </section>
+            </div>
+        </TasksDispatchContext.Provider>
     )
+
 }
 
 export default NotificationsPage
 
+export function useDispatch() {
+    return useContext(TasksDispatchContext)
+}
